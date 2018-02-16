05ba825795f88fbd3104beea8f36df483d0f0658
adrianacosta20 <aacosta@techlaunch.io>
2018-02-15 18:45:26 -0800

    initial Commit

---
+++ quiz.js
@@ -0,0 +1,40 @@
+const workers = [
+    { name: 'Adrian Acosta', hourlyRate: 10.50, hoursWorked: 50 },
+    { name: 'Ines Hernandez', hourlyRate: 10.50, hoursWorked: 70 },
+    { name: 'Gabriel Luciano', hourlyRate: 10.50, hoursWorked: 100 },
+
+]
+
+class Payroll {
+    constructor(workers) {
+        this.workers = workers
+        this.orderName = this.getNames(this.workers)
+        this.fullTime = this.getFullTime(this.workers)
+        this.average = this.getTotalLabor(this.workers)
+
+    }
+    getNames(workers) {
+        return workers.map(n => {
+            return n.name
+            
+        }
+    )
+    return orderName.sort()
+        
+
+
+    }
+
+    getFullTime(workers) {
+        return workers.filter(n => {
+            return (n.hoursWorked > 59) ? 1 : 0
+        })
+    }
+    getTotalLabor(workers) {
+        return workers.reduce((acc, curr) => {
+            return acc + (curr * acc)
+        })
+    }
+}
+payroll = new Payroll(workers)
+console.log(payroll.orderName)
